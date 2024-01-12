3a3403f94f6157ffe39f8166de074b9cf053f3a2
nonergodic <nonergodic@gmail.com>
2024-01-11 16:08:07 -0800

    finish implementing lion's share of layouts

--- ts-sdk/index.ts
+++ ts-sdk/index.ts
@@ -8,7 +8,13 @@ import {
   proxyConstructorArgsLayout,
   governanceCommandLayout,
   governanceCommandsBatchLayout,
+  initiateArgsLayout,
   swapMessageLayout,
+  feeParamsLayout,
+  feeParamUpdateLayout,
+  feeParamUpdatesBatchLayout,
+  queryLayout,
+  queriesBatchLayout,
 } from "./layout";
 
 export type ProxyConstructorArgs = LayoutToType<typeof proxyConstructorArgsLayout>;
@@ -19,6 +25,23 @@ export type GovernanceCommand = LayoutToType<typeof governanceCommandLayout>;
 export const encodeGovernanceCommandsBatch = (args: readonly GovernanceCommand[]): Uint8Array =>
   serializeLayout(governanceCommandsBatchLayout, args);
 
+export type InitiateArgs = LayoutToType<typeof initiateArgsLayout>;
+export const encodeInitiateArgs = (args: InitiateArgs): Uint8Array =>
+  serializeLayout(initiateArgsLayout, args);
+
+export type FeeParamUpdate = LayoutToType<typeof feeParamUpdateLayout>;
+export const encodeFeeParamUpdatesBatch = (args: readonly FeeParamUpdate[]): Uint8Array =>
+  serializeLayout(feeParamUpdatesBatchLayout, args);
+
+//combine with adhoc layouts to deserialize return values
+export type Query = LayoutToType<typeof queryLayout>;
+export const encodeQueriesBatch = (args: readonly Query[]): Uint8Array =>
+  serializeLayout(queriesBatchLayout, args);
+
 export type SwapMessage = LayoutToType<typeof swapMessageLayout>;
 export const deserializeSwapMessage = (data: Uint8Array): SwapMessage =>
   deserializeLayout(swapMessageLayout, data);
+
+export type FeeParams = LayoutToType<typeof feeParamsLayout>;
+export const deserializeFeeParams = (data: Uint8Array): FeeParams =>
+  deserializeLayout(feeParamsLayout, data);
--- ts-sdk/layout.ts
+++ ts-sdk/layout.ts
@@ -9,6 +9,13 @@ import {
 import { layoutItems } from "@wormhole-foundation/sdk-definitions"
 import { EvmAddress } from "@wormhole-foundation/connect-sdk-evm";
 
+const forceBigIntConversion = {
+  custom: {
+    to: (val: number) => BigInt(val),
+    from: (val: bigint) => Number(val),
+  } satisfies CustomConversion<number, bigint>,
+} as const;
+
 //I can't believe I forgot to implement this!
 const boolItem = {
   binary: "uint",
@@ -63,13 +70,8 @@ export const governanceCommandsBatchLayout = {
   layout: governanceCommandLayout,
 } as const satisfies Layout;
 
-const deadlineItem = {
-  name: "deadline",
-  binary: "uint",
-  size: 4
-} as const satisfies NamedLayoutItem;
-
-const amountItem = { binary: "uint", size: 16 } as const satisfies UintLayoutItem;
+const timestampItem = { binary: "uint", size:  4 } as const satisfies UintLayoutItem;
+const amountItem    = { binary: "uint", size: 16 } as const satisfies UintLayoutItem;
 
 const fastTransferModeItem = {
   name: "fastTransferMode",
@@ -79,11 +81,21 @@ const fastTransferModeItem = {
   layouts: [
     [[0, "Disabled"], []],
     [[1, "Enabled"], [
-      { name: "maxFee", binary: "uint", size: 6, /*todo max fee conversion*/ },
-      deadlineItem,
+      { name: "maxFee", binary: "uint", size: 6, ...forceBigIntConversion }, //atomic usdc
+      { name: "deadline", ...timestampItem }, //according to block timestamp
     ]],
   ]
 } as const satisfies NamedLayoutItem;
+
+const gasDropoffResolution = BigInt(1e6);
+const gasDropoffItem = {
+  binary: "uint",
+  size: 4,
+  custom: {
+    to: (val: number): bigint => BigInt(val) * gasDropoffResolution,
+    from: (price: bigint): number => Number(price / gasDropoffResolution),
+  } as const satisfies CustomConversion<number, bigint>,
+} as const satisfies UintLayoutItem;
 
 const redeemModeItem = {
   name: "redeemMode",
@@ -94,8 +106,8 @@ const redeemModeItem = {
     [[0, "Direct"], []],
     [[1, "Payload"], [{ name: "payload", binary: "bytes", lengthSize: 4 }]],
     [[2, "Relay"], [
-      { name: "gasDropoff", binary: "uint", size: 4, /*todo gas dropoff conversion*/ },
-      { name: "relayerFee", binary: "uint", size: 6, /*todo relayer fee conversion*/ }
+      { name: "gasDropoff", ...gasDropoffItem },
+      { name: "maxRelayingFee", binary: "uint", size: 6, ...forceBigIntConversion },
     ]],
   ]
 } as const satisfies NamedLayoutItem;
@@ -128,11 +140,11 @@ const acquireModeItem = {
   ]
 } as const satisfies NamedLayoutItem;
 
-export const swapLayout = [
+const swapLayout = [
   //this will eventually be a switch - but for now we only have one type - uniswap
   { name: "type", binary: "uint", size: 1, custom: { to: "Uniswap", from: 1 }},
   { name: "limitAmount", ...amountItem },
-  deadlineItem,
+  { name: "deadline", ...timestampItem },
   { name: "legFirstFee", binary: "uint", size: 3 },
   { name: "path", binary: "array", lengthSize: 1, layout: [
     { name: "address", ...evmAddressItem },
@@ -193,3 +205,147 @@ export const swapMessageLayout = [
   redeemModeItem,
   outputTokenItem,
 ] as const satisfies Layout;
+
+const percentageItem = {
+  binary: "uint",
+  size: 2,
+  custom: {
+    to: (val: number): number => (val>>2) / 10**(2 + (val % 4)),
+    from: (percentage: number): number => {
+      if (percentage > 100 || percentage < 0)
+        throw new Error("Percentage must be between 0 and 100");
+      
+      if (percentage < 1e-5)
+        return 0;
+      
+      let negexp = Math.min(3, Math.floor(-Math.log10(percentage)) + 2);
+      let mantissa = Math.round(percentage * 10**(2 + negexp));
+      while (mantissa % 10 == 0 && negexp > 0) {
+        mantissa /= 10;
+        --negexp;
+      }
+      return (mantissa << 2) + negexp;
+    }
+  } satisfies CustomConversion<number, number>,
+} as const satisfies UintLayoutItem;
+
+const gasPriceResolution = BigInt(1e6);
+const gasPriceItem = {
+  binary: "uint",
+  size: 4,
+  custom: {
+    to: (val: number): bigint => BigInt(val) * gasPriceResolution,
+    from: (price: bigint): number => Number(price / gasPriceResolution),
+  }
+} as const satisfies UintLayoutItem;
+
+const baseFeeItem = {
+  binary: "uint",
+  size: 4,
+  ...forceBigIntConversion
+} as const satisfies UintLayoutItem;
+
+const gasTokenPriceItem = { binary: "uint", size: 10 } as const satisfies UintLayoutItem;
+
+export const feeParamsLayout = [
+  { name: "baseFee",                 ...baseFeeItem       },
+  { name: "gasPrice",                ...gasPriceItem      },
+  { name: "gasPriceMargin",          ...percentageItem    },
+  { name: "gasPriceTimestamp",       ...timestampItem     },
+  { name: "gasPriceUpdateThreshold", ...percentageItem    },
+  { name: "maxGasDropoff",           ...gasDropoffItem    },
+  { name: "gasDropoffMargin",        ...percentageItem    },
+  { name: "gasTokenPrice",           ...gasTokenPriceItem },
+] as const satisfies Layout;
+
+export const feeParamUpdateItem = {
+  binary: "switch",
+  idSize: 1,
+  idTag: "param",
+  layouts: [
+    [[0, "GasPrice"],                [{ name: "value", ...addressChainItem  }]],
+    [[1, "GasTokenPrice"],           [{ name: "value", ...gasTokenPriceItem }]],
+    [[2, "BaseFee"],                 [{ name: "value", ...baseFeeItem       }]],
+    [[3, "GasPriceUpdateThreshold"], [{ name: "value", ...percentageItem    }]],
+    [[4, "GasPriceMargin"],          [{ name: "value", ...percentageItem    }]],
+    [[5, "GasDropoffMargin"],        [{ name: "value", ...percentageItem    }]],
+    [[6, "MaxGasDropoff"],           [{ name: "value", ...gasDropoffItem    }]],
+  ],
+} as const satisfies LayoutItem;
+
+export const feeParamUpdateLayout = [
+  { name: "chain", ...layoutItems.chainItem() },
+  { name: "update", ... feeParamUpdateItem },
+] as const satisfies Layout;
+
+export const feeParamUpdatesBatchLayout = {
+  binary: "array",
+  layout: feeParamUpdateLayout,
+} as const satisfies Layout;
+
+const immutableTypes = [
+  "Wormhole",
+  "Usdc",
+  "Weth",
+  "Permit2",
+  "UniswapV3Router",
+  "LiquidityLayer",
+  "MajorDelay",
+  "MinorDelay"
+] as const;
+type ImmutableType = typeof immutableTypes[number];
+const immutableTypeItem = {
+  binary: "uint",
+  size: 1,
+  custom: {
+    to: (val: number) => {
+      if (val >= immutableTypes.length)
+        throw new Error("Invalid immutable type");
+
+      return immutableTypes[val];
+    },
+    from: (val: ImmutableType): number => immutableTypes.indexOf(val),
+  }
+} as const satisfies UintLayoutItem;
+
+const subQueries = ["Current", "Proposed", "UnlockTime"] as const;
+type SubQuery = typeof subQueries[number];
+const subQueryTypeItem = {
+  binary: "uint",
+  size: 1,
+  custom: {
+    to: (val: number) => {
+      if (val >= subQueries.length)
+        throw new Error("Invalid subquery type");
+
+      return subQueries[val];
+    },
+    from: (val: SubQuery): number => subQueries.indexOf(val),
+  }
+} as const satisfies UintLayoutItem;
+
+export const queryLayout = {
+  binary: "switch",
+  idSize: 1,
+  idTag: "query",
+  layouts: [
+    //TODO define remaining GovernanceCommand types
+    [[0, "FeeParams"],               [{ name: "chain", ...layoutItems.chainItem() }]],
+    [[1, "Endpoint"],                [
+                                      { name: "subQuery", ...subQueryTypeItem },
+                                      { name: "chain", ...layoutItems.chainItem() }
+                                     ]],
+    [[2, "Immutable"],               [{ name: "immutable", ...immutableTypeItem }]],
+    [[3, "AdminCanUpgradeContract"], []],
+    [[4, "Assistant"],               []],
+    [[5, "Owner"],                   [{ name: "subQuery", ...subQueryTypeItem }]],
+    [[6, "Admin"],                   [{ name: "subQuery", ...subQueryTypeItem }]],
+    [[7, "FeeRecipient"],            [{ name: "subQuery", ...subQueryTypeItem }]],
+    [[8, "Implementation"],          [{ name: "subQuery", ...subQueryTypeItem }]],
+  ],
+} as const satisfies LayoutItem;
+
+export const queriesBatchLayout = {
+  binary: "array",
+  layout: queryLayout,
+} as const satisfies Layout;
