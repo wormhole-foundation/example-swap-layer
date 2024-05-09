d42884a7753f473f63fb42524b5b7246f3bc5edc
nonergodic <nonergodic@gmail.com>
2024-05-09 02:05:30 -0700

    add substantial checks to first initiate test case

--- evm/test/Initiate.t.sol
+++ evm/test/Initiate.t.sol
@@ -2,7 +2,14 @@
 
 pragma solidity ^0.8.24;
 
+//TODO remove
+import {console2 as console} from "forge-std/console2.sol";
+
+import { WormholeCctpMessages } from "wormhole-sdk/libraries/WormholeCctpMessages.sol";
 import { toUniversalAddress } from "wormhole-sdk/Utils.sol";
+import { WormholeOverride, PublishedMessage } from "wormhole-sdk/testing/WormholeOverride.sol";
+import { Messages as LiquidityLayerMessages } from "liquidity-layer/shared/Messages.sol";
+import { SwapMessageStructure, parseSwapMessageStructure } from "swap-layer/assets/Message.sol";
 
 import "swap-layer/SwapLayerIntegrationBase.sol";
 
@@ -10,6 +17,7 @@ import "./SLTSwapBase.sol";
 
 contract InitiateTest is SLTSwapBase, SwapLayerIntegrationBase {
   using BytesParsing for bytes;
+  using WormholeOverride for IWormhole;
   using { toUniversalAddress } for address;
 
   function _swapLayer() override internal view returns (ISwapLayer) {
@@ -18,18 +26,53 @@ contract InitiateTest is SLTSwapBase, SwapLayerIntegrationBase {
 
   function testInitiateDirectUsdc() public {
     uint amount = USER_AMOUNT * 1e6;
+    bytes32 recipient = user.toUniversalAddress();
     _dealOverride(address(usdc), user, amount);
     vm.startPrank(user);
     usdc.approve(address(swapLayer), amount);
+
+    vm.recordLogs();
+
     (uint amountOut, , ) = _swapLayerInitiate(InitiateUsdc({
-      targetParams: TargetParams(
-        FOREIGN_CHAIN_ID,
-        user.toUniversalAddress()
-      ),
+      targetParams: TargetParams(FOREIGN_CHAIN_ID, recipient),
       amount: amount,
       outputParams: _swapLayerEncodeOutputParamsUsdc()
     }));
-    assertEq(amount, amountOut);
+    assertEq(amountOut, amount);
+    assertEq(usdc.balanceOf(user), 0);
+
+    PublishedMessage[] memory pubMsgs = wormhole.fetchPublishedMessages(vm.getRecordedLogs());
+    assertEq(pubMsgs.length, 1);
+    (
+      bytes32 token,
+      uint256 cctpAmount,
+      , //uint32 sourceCctpDomain,
+      , //uint32 targetCctpDomain,
+      , //uint64 cctpNonce,
+      , //bytes32 burnSource,
+      bytes32 mintRecipient,
+      bytes memory payload
+    ) = WormholeCctpMessages.decodeDeposit(pubMsgs[0].payload);
+
+    assertEq(token, address(usdc).toUniversalAddress());
+    assertEq(cctpAmount, amount);
+    assertEq(mintRecipient, FOREIGN_LIQUIDITY_LAYER);
+
+    LiquidityLayerMessages.Fill memory fill = LiquidityLayerMessages.decodeFill(payload);
+    assertEq(fill.orderSender, address(swapLayer).toUniversalAddress());
+    assertEq(fill.redeemer, FOREIGN_SWAP_LAYER);
+    SwapMessageStructure memory swapMessageStructure =
+      parseSwapMessageStructure(fill.redeemerMessage);
+
+    assertEq(swapMessageStructure.recipient, user);
+    assertEq(uint8(swapMessageStructure.redeemMode), uint8(RedeemMode.Direct));
+    assertEq(swapMessageStructure.payload.length, 0);
+    (IoToken outputToken, ) = parseIoToken(fill.redeemerMessage, swapMessageStructure.swapOffset);
+    assertEq(uint8(outputToken), uint8(IoToken.Usdc));
+  }
+
+  function testPausedRelay() public {
+
   }
 
   function testInitiateTraderJoeEthSwap() public {
