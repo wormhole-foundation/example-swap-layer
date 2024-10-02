1bc0f385318ca7925805636de5fb29b7b3ec30a5
A5 Pickle <a5-pickle@users.noreply.github.com>
2024-10-02 12:14:58 -0500

    evm: fix wormhole-solidity-sdk commit
    evm: fix imports

--- evm/src/assets/SwapLayerInitiate.sol
+++ evm/src/assets/SwapLayerInitiate.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/token/ERC20/IERC20.sol";
 import "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/token/ERC20/extensions/IERC20Permit.sol";
 
-import "wormhole-sdk/interfaces/token/IPermit2.sol";
+import "permit2/interfaces/IPermit2.sol";
 import { BytesParsing } from "wormhole-sdk/libraries/BytesParsing.sol";
 
 import { checkAddr } from "./Params.sol";
